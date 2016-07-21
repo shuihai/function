e2166f69108e474500f804eb5b82f66976b0c1d2
shuihai <99359473@qq.com>
2016-07-21 16:59:48 +0800

    Update function.php

--- function.php
+++ function.php
@@ -1,3 +1,10 @@
+sql语句
+$data = $user->query("select u.id,IF(u.portrait != '',CONCAT('http://".$_SERVER['HTTP_HOST'].__APP__."',u.portrait),'')  from user as u;
+
+模型的query和execute方法 同样支持预处理机制，例如：
+$model->query('select * from user where id=%d and status=%d',$id,$status);//或者$model->query('select * from user where id=%d and status=%d',array($id,$status));
+
+
 /*告诉浏览器utf8编码的反应头*/
 header('Content-Type:text/html;charset=utf-8');
 
