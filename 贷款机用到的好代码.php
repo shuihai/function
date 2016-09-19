eddeb526faefca16c439761610c22d0e8ac5fafd
shuihai <99359473@qq.com>
2016-09-19 10:48:07 +0800

    Update 贷款机用到的好代码.php

--- 贷款机用到的好代码.php
+++ 贷款机用到的好代码.php
@@ -1,3 +1,4 @@
+ 
  //省市区三级联动
     var cityurl = "{:U('getcity')}";
     var districturl = "{:U('getdistrict')}";
@@ -17,11 +18,41 @@
                 },'json')
     省略
     
+    
+    
  //在指定DIV下的第二个P后面增加元素如何实现
 <script type="text/javascript">
     var insertHtml='<div>我是插入的元素。</div>'
     $('#divId').find('p').eq(-2).after(insertHtml);
 </script>
 
+
+
 //js生成url记得编码
 phoneurl=encodeURI(encodeURI(phoneurl));
+
+
+
+//序列化表单数据并用ajax交互
+            $('#save').on('click',function(){
+            
+               $.ajax({
+                   url:"<?=\yii\helpers\Url::toRoute('/organization/essentialadd')?>",
+                   type:'post',
+                   data:$('#employer_form').serialize(),
+                   success:function(data){
+                         //注意这里
+                        data = JSON.parse(data);
+                        if(data.code == 200){
+                    
+                            $('#id').val(data);
+                            $("#essential-center").find("input,select,textarea").attr( "disabled", true).css("cursor", "default");
+                            $("#delete").show();
+                            $('#deit').show();
+                            $('#save').hide();
+                        }else{
+                            alert(data.info);
+                        }
+                   }
+               });
+            });
